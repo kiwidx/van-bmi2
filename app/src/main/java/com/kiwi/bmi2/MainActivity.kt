82b4c0af658fa2a4af5c49ea6aa376c80392e391
kiwi <davincidx0418@gmail.com>
2022-03-03 17:27:53 +0800

    show the bmi in logcat but it failed

--- app/src/main/java/com/kiwi/bmi2/MainActivity.kt
+++ app/src/main/java/com/kiwi/bmi2/MainActivity.kt
@@ -2,10 +2,23 @@ package com.kiwi.bmi2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.kiwi.bmi2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding  //lateinit告訴機器這個變數的值晚一點給，不會是null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)  //inflate類似給binding設定初值
+        setContentView(binding.root)    //到binding的根目錄拿值
         setContentView(R.layout.activity_main)
+    }
+
+    fun bmi(view: View){
+        var weight = binding.edWeight.text.toString().toFloat()
+        var height = binding.edWeight.text.toString().toFloat()
+        val bmi = weight /(height*height)
+        Log.d("BMI", bmi.toString())  //彈出對話視窗顯示結果
     }
 }
